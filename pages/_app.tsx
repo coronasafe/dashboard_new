c07751c3bcb1ae75e033ede6859aab2034f84c2b
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-07-22 20:09:50 +0530

    🛠 Add Basic Configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,30 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { Windmill } from "@windmill/react-ui";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Windmill>
+      <Head>
+        <meta charSet="utf-8" />
+        <link
+          rel="icon"
+          href="https://cdn.coronasafe.network/care-manifest/images/icons/icon-192x192.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#0e9f6e" />
+        <meta
+          name="description"
+          content="A data visualization dashboard with capacity map, plots and charts of districts."
+        />
+        <link
+          rel="apple-touch-icon"
+          href="https://cdn.coronasafe.network/care-manifest/images/icons/icon-192x192.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <title>Care Dashboard</title>
+      </Head>
       <Component {...pageProps} />
     </Windmill>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,39 @@
 const windmill = require('@windmill/react-ui/config')
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = windmill({
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          100: "#def7ec",
+          200: "#bcf0da",
+          300: "#84e1bc",
+          400: "#31c48d",
+          500: "#0e9f6e",
+          600: "#057a55",
+          700: "#046c4e",
+          800: "#03543f",
+          900: "#014737",
+        },
+        gray: {
+          100: "#F4F4F5",
+          200: "#E4E4E7",
+          300: "#D4D4D8",
+          400: "#A1A1AA",
+          500: "#71717A",
+          600: "#52525B",
+          700: "#3F3F46",
+          800: "#27272A",
+          900: "#18181B",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
