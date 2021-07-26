538ac00f538d208f6159421fc88d4de8cecd6efa
developedBySJ <developedbysj@gmail.com>
2021-07-26 12:51:46 +0530

    Use Radial Chart

--- pages/district/[districtName]/capacity.tsx
+++ pages/district/[districtName]/capacity.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { RadialCard } from "../../../components/Charts";
 import ContentNav from "../../../components/ContentNav";
 import { GetDistrictName } from "../../../utils/parser";
 
@@ -10,6 +11,14 @@ const Capacity = () => {
   return (
     <div className="container mx-auto px-4">
       <ContentNav />
+      <div className="w-1/2 sm:w-1/2 md:w-1/4 lg:w-1/5">
+        <RadialCard
+          count={20}
+          current={{ total: 100, used: 50 }}
+          label="Non-Covid Oxygen Beds"
+          previous={{ total: 100, used: 49 }}
+        />
+      </div>
     </div>
   );
 };
