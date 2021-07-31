8fba1a056dc00d2b26f98f86fb35f820557461bb
developedBySJ <developedbysj@gmail.com>
2021-07-31 10:06:19 +0530

    Add section

--- pages/district/[districtName]/capacity.tsx
+++ pages/district/[districtName]/capacity.tsx
@@ -48,88 +48,93 @@ const Capacity: React.FC<CapacityProps> = ({ data, districtName }) => {
   return (
     <div className="2xl:container mx-auto px-4">
       <ContentNav />
-      <div className="grid gap-1 grid-rows-none mb-8 sm:grid-flow-col-dense sm:grid-rows-1 sm:place-content-end my-5">
-        <ValuePill
-          title="Facility Count"
-          value={facilitiesTrivia.current.count}
-        />
-        <ValuePill
-          title="Oxygen Capacity"
-          value={facilitiesTrivia.current.oxygen}
-        />
-        <ValuePill
-          title="Live Patients"
-          value={facilitiesTrivia.current.actualLivePatients}
-        />
-        <ValuePill
-          title="Discharged Patients"
-          value={facilitiesTrivia.current.actualDischargedPatients}
-        />
-        <Pill title="Forecast">
-          <Button size="small" className="bg-transparent shadow-xs w-full">
-            <ArrowRight className="h-4" />
-          </Button>
-        </Pill>
-      </div>
-
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 my-5">
-        {AVAILABILITY_TYPES_TOTAL_ORDERED.map((k) => (
-          <RadialCard
-            label={k.name}
-            count={facilitiesTrivia.current.count}
-            //@ts-ignore
-            current={facilitiesTrivia.current[k.id]}
-            //@ts-ignore
-            previous={facilitiesTrivia.previous[k.id]}
-            key={k.id}
+      <section id="charts">
+        <div className="grid gap-1 grid-rows-none mb-8 sm:grid-flow-col-dense sm:grid-rows-1 sm:place-content-end my-5">
+          <ValuePill
+            title="Facility Count"
+            value={facilitiesTrivia.current.count}
           />
-        ))}
-        {AVAILABILITY_TYPES_ORDERED.map((k) => (
-          <RadialCard
-            label={AVAILABILITY_TYPES[k]}
-            count={facilitiesTrivia.current.count}
-            //@ts-ignore
-            current={facilitiesTrivia.current[k]}
-            //@ts-ignore
-            previous={facilitiesTrivia.previous[k]}
-            key={k}
+          <ValuePill
+            title="Oxygen Capacity"
+            value={facilitiesTrivia.current.oxygen}
           />
-        ))}
-      </div>
-      <div id="facility-capacity-cards" className="mb-16 mt-16">
-        <div className="items-center flex flex-col justify-between md:flex-row">
-          <h1 className="dark:text-gray-100 text-3xl mb-4">Facilities</h1>
-          <div className="flex max-w-full space-x-4">
-            {true && (
-              // <CSVLink data={exported.data} filename={exported.filename}>
-              <Button block>Export</Button>
-              // </CSVLink>
-            )}
-            <Input
-              css={{}}
-              className="sw-40 rounded-lg sm:w-auto"
-              placeholder="Search Facility"
-              value={searchTerm}
-              onChange={(event) => setSearchTerm(event.target.value)}
-            />
-          </div>
+          <ValuePill
+            title="Live Patients"
+            value={facilitiesTrivia.current.actualLivePatients}
+          />
+          <ValuePill
+            title="Discharged Patients"
+            value={facilitiesTrivia.current.actualDischargedPatients}
+          />
+          <Pill title="Forecast">
+            <Button size="small" className="bg-transparent shadow-xs w-full">
+              <ArrowRight className="h-4" />
+            </Button>
+          </Pill>
         </div>
 
-        {tableData.map((tData, index) => (
-          <CapacityCard data={tData} key={index} />
-        ))}
+        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 my-5">
+          {AVAILABILITY_TYPES_TOTAL_ORDERED.map((k) => (
+            <RadialCard
+              label={k.name}
+              count={facilitiesTrivia.current.count}
+              //@ts-ignore
+              current={facilitiesTrivia.current[k.id]}
+              //@ts-ignore
+              previous={facilitiesTrivia.previous[k.id]}
+              key={k.id}
+            />
+          ))}
+          {AVAILABILITY_TYPES_ORDERED.map((k) => (
+            <RadialCard
+              label={AVAILABILITY_TYPES[k]}
+              count={facilitiesTrivia.current.count}
+              //@ts-ignore
+              current={facilitiesTrivia.current[k]}
+              //@ts-ignore
+              previous={facilitiesTrivia.previous[k]}
+              key={k}
+            />
+          ))}
+        </div>
+      </section>
+      <section id="facility-capacity">
+        <div id="facility-capacity-cards" className="mb-16 mt-16">
+          <div className="items-center flex flex-col justify-between md:flex-row">
+            <h1 className="dark:text-gray-100 text-3xl mb-4">Facilities</h1>
+            <div className="flex max-w-full space-x-4">
+              {true && (
+                // <CSVLink data={exported.data} filename={exported.filename}>
+                <Button block>Export</Button>
+                // </CSVLink>
+              )}
+              <Input
+                css={{}}
+                className="sw-40 rounded-lg sm:w-auto"
+                placeholder="Search Facility"
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+              />
+            </div>
+          </div>
 
-        <Pagination
-          resultsPerPage={10}
-          totalResults={capacityCardData.length}
-          label=""
-          onChange={(page) => setPage(page)}
-        />
-      </div>
-      <div id="capacity-map">
+          {tableData.map((tData, index) => (
+            <CapacityCard data={tData} key={index} />
+          ))}
+
+          <Pagination
+            resultsPerPage={10}
+            totalResults={capacityCardData.length}
+            label=""
+            onChange={(page) => setPage(page)}
+          />
+        </div>
+      </section>
+      <section id="capacity-map">
         <h1 className="dark:text-gray-100 text-3xl mb-4"> Map </h1>
-      </div>
-      <GMap district={filterDistrict} facilities={todayFiltered} />
+
+        <GMap district={filterDistrict} facilities={todayFiltered} />
+      </section>
     </div>
   );
 };
