1010ebec6c3930ae8fa8a532cf313257232193e3
developedBySJ <developedbysj@gmail.com>
2021-08-12 17:04:08 +0530

    Refactor

--- components/Patient/PatientSummary.tsx
+++ components/Patient/PatientSummary.tsx
@@ -3,7 +3,7 @@ import useSWR from "swr";
 import { PATIENT_TYPES } from "../../lib/common";
 import { PatientSummary } from "../../lib/types";
 import { District } from "../../lib/types/common";
-import PatientSummaryCard from "./PatientSummaryCard";
+import { InfoCard } from "../InfoCard";
 
 export interface PatientSummaryProps {
   district: District;
@@ -19,7 +19,7 @@ const PatientSummaryPage: React.FC<PatientSummaryProps> = ({ district }) => {
   return (
     <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-8 ">
       {Object.entries(PATIENT_TYPES).map(([key, value], i) => (
-        <PatientSummaryCard
+        <InfoCard
           key={i}
           title={value}
           value={(data && data[key]?.total) || 0}
