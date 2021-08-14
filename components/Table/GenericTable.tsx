691ee53e8d4c20a9dfb9a226519faf31e9b8987b
developedBySJ <developedbysj@gmail.com>
2021-08-14 14:56:16 +0530

    Update GenericTableProps

--- components/Table/GenericTable.tsx
+++ components/Table/GenericTable.tsx
@@ -1,29 +1,24 @@
 import React from "react";
 import Table from "rc-table";
-import {
-  ColumnsType,
-  DefaultRecordType,
-  TableComponents,
-} from "rc-table/lib/interface";
+import { ColumnsType, DefaultRecordType } from "rc-table/lib/interface";
 
 interface GenericTableProps {
   data: DefaultRecordType[];
   columns: ColumnsType<DefaultRecordType>;
+  scroll: {
+    x?: string | number | true;
+    y?: string | number;
+  };
 }
 
 export const GenericTable: React.FC<GenericTableProps> = ({
   data,
   columns,
+
+  scroll = {
+    x: 1500,
+    y: process.browser ? window?.innerHeight : undefined,
+  },
 }) => {
-  return (
-    <Table
-      data={data}
-      columns={columns}
-      scroll={{
-        x: 1500,
-        y: process.browser ? window?.innerHeight : undefined,
-      }}
-      sticky={true}
-    />
-  );
+  return <Table data={data} columns={columns} scroll={scroll} sticky={true} />;
 };
