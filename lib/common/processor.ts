5a05222baae82d3783f662fbae4f27311d4b5574
Karan Sanjeev <yo@alphaman.me>
2021-08-18 14:26:45 +0530

    Strip out data processor part from capacity

--- lib/common/processor.ts
+++ lib/common/processor.ts
@@ -1,6 +1,20 @@
-import { OXYGEN_INVENTORY } from ".";
+import {
+  AVAILABILITY_TYPES,
+  AVAILABILITY_TYPES_TOTAL_ORDERED,
+  OXYGEN_INVENTORY,
+} from ".";
 import { FacilityData, FacilitySummary, Inventory } from "../types";
-import { InventoryTimeToEmpty, toDateString } from "../../utils/parser";
+import {
+  getCapacityBedData,
+  getFinalTotalData,
+  InventoryTimeToEmpty,
+  toDateString,
+} from "../../utils/parser";
+import { INITIAL_FACILITIES_TRIVIA } from "./constants";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 export const processFacilityData = (facilities: FacilitySummary[]) => {
   const facility = facilities.filter((obj) => obj.facility);
@@ -85,4 +99,71 @@ export const processFacilityData = (facilities: FacilitySummary[]) => {
       ...additionalData(data),
     };
   });
+};
+
+export const processFacilityTrivia = (facility: typeof processFacilityData) => {
+  const date = new Date();
+  // @ts-ignore
+  return facility.reduce(
+    // @ts-ignore
+    (a, c) => {
+      const key = c.date === toDateString(date) ? "current" : "previous";
+      a[key].count += 1;
+      a[key].oxygen += c.oxygenCapacity || 0;
+      a[key].actualLivePatients += c.actualLivePatients || 0;
+      a[key].actualDischargedPatients += c.actualDischargedPatients || 0;
+      Object.keys(AVAILABILITY_TYPES).forEach((k) => {
+        a[key][k].used += c.capacity[k]?.current_capacity || 0;
+        a[key][k].total += c.capacity[k]?.total_capacity || 0;
+      });
+
+      AVAILABILITY_TYPES_TOTAL_ORDERED.forEach((k) => {
+        const current_covid = c.capacity[k.covid]?.current_capacity || 0;
+        const current_non_covid =
+          c.capacity[k.non_covid]?.current_capacity || 0;
+        const total_covid = c.capacity[k.covid]?.total_capacity || 0;
+        const total_non_covid = c.capacity[k.non_covid]?.total_capacity || 0;
+        a[key][k.id].used += current_covid + current_non_covid;
+        a[key][k.id].total += total_covid + total_non_covid;
+      });
+
+      return a;
+    },
+    {
+      current: JSON.parse(JSON.stringify(INITIAL_FACILITIES_TRIVIA)),
+      previous: JSON.parse(JSON.stringify(INITIAL_FACILITIES_TRIVIA)),
+    }
+  );
+};
+
+export const processCapacityCardData = (
+  facilities: typeof processFacilityData
+) => {
+  const date = new Date();
+  // @ts-ignore
+  return facilities.reduce((acc, facility) => {
+    const covidData = getCapacityBedData([30, 120, 110, 100], facility);
+    const nonCovidData = getCapacityBedData([1, 150, 10, 20], facility);
+    const finalTotalData = getFinalTotalData(covidData, nonCovidData);
+    const noCapacity = finalTotalData.every((item) => item.total === 0);
+    if (facility.date !== toDateString(date) || noCapacity) {
+      return acc;
+    }
+    return [
+      ...acc,
+      {
+        facility_name: facility.name,
+        facility_id: facility.id,
+        facility_type: facility.facilityType,
+        phone_number: facility.phoneNumber,
+        last_updated: dayjs(facility.modifiedDate).fromNow(),
+        patient_discharged: `${facility.actualLivePatients || 0}/${
+          facility.actualDischargedPatients || 0
+        }`,
+        covid: covidData,
+        non_covid: nonCovidData,
+        final_total: finalTotalData,
+      },
+    ];
+  }, []);
 };
