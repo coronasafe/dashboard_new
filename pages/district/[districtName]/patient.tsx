659dbeb3edf3c7bc4baada978bca48491d4e4e12
developedBySJ <developedbysj@gmail.com>
2021-09-02 12:03:07 +0530

    Use Server Side data

--- pages/district/[districtName]/patient.tsx
+++ pages/district/[districtName]/patient.tsx
@@ -6,13 +6,13 @@ import {
   getDistrict,
   getNDateAfter,
   getNDateBefore,
+  parameterize,
   toDateString,
 } from "../../../utils/parser";
 import Loader from "../../../lib/assets/icons/LoaderIcon";
 import { TableExportHeader } from "../../../components/TableExportHeader";
 import { BedsSummery } from "../../../components/Patient/BedsSummery";
-import useSWR from "swr";
-import { careSummary } from "../../../lib/types";
+import { careSummary, CareSummaryResponse } from "../../../lib/types";
 import {
   ACTIVATED_DISTRICTS,
   FACILITY_TYPES,
@@ -29,25 +29,17 @@ import {
   processPatientFacilitiesTriviaData,
 } from "../../../lib/common/processor/patientProcessor";
 import { InfoCard } from "../../../components/InfoCard";
-import { Box } from "react-feather";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import _ from "lodash";
+import { Pagination } from "@windmill/react-ui";
 
-const Patient = () => {
+const Patient = ({ data }: { data: CareSummaryResponse }) => {
   const router = useRouter();
   const districtName = router.query.districtName;
   const district = getDistrict(districtName?.toString());
   const [facilityType, setFacilityType] = useState(FACILITY_TYPES);
   const [date, dateOnChange] = useState(new Date());
   const [filterDistrict, setFilterDistrict] = useState(ACTIVATED_DISTRICTS[0]);
-
-  const startDate = toDateString(getNDateBefore(date, 1));
-  const endDate = toDateString(getNDateAfter(date, 1));
-  const limit = 2000;
-
-  const { data } = useSWR(
-    ["Patient", date, filterDistrict.id],
-    (url, date: Date, districtId: string) =>
-      careSummary("patient", districtId, limit, startDate, endDate)
-  );
 
   if (!data || !district) return <Loader />;
   const [tableData, setTableData] = useState<PatientCardDataForCapacity[]>([]);
@@ -64,7 +56,7 @@ const Patient = () => {
       filteredData.slice(page * resultsPerPage, (page + 1) * resultsPerPage)
     );
   }, [filteredData, page]);
-
+  console.log({ tableData });
   return (
     <div className="container mx-auto px-4">
       <ContentNav />
@@ -74,7 +66,6 @@ const Patient = () => {
           value={facilityTrivia.current.count}
         />
       </div>
-      {/* {district && <PatientSummaryPage district={district} />} */}
       <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-8 ">
         {Object.entries(PATIENT_TYPES).map(([k, title], i) => {
           const key = k as PatientTypeKeys;
@@ -83,7 +74,7 @@ const Patient = () => {
           return <InfoCard key={i} title={title} value={value} delta={delta} />;
         })}
       </div>
-      <Box>
+      <div className="my-16">
         <TableExportHeader
           label="Facilities"
           searchValue=""
@@ -92,9 +83,86 @@ const Patient = () => {
         {tableData.map((data, i) => (
           <BedsSummery key={i} data={data} />
         ))}
-      </Box>
+        <div className="my-4 py-4">
+          <Pagination
+            label="Bed Summery"
+            resultsPerPage={10}
+            totalResults={filteredData.length}
+            onChange={(page) => setPage(page)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Patient;
+
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+}: GetServerSidePropsContext) => {
+  const district = _.find(
+    ACTIVATED_DISTRICTS,
+    (obj) =>
+      parameterize(obj.name) === parameterize(params?.districtName as string)
+  );
+
+  if (!district) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const today = new Date();
+  const start_date = toDateString(getNDateBefore(today, 1));
+  const end_date = toDateString(getNDateAfter(today, 1));
+  const limit = 2000;
+
+  console.log({ start_date, end_date });
+  const data = await careSummary(
+    "patient",
+    district.id,
+    limit,
+    start_date,
+    end_date
+  );
+
+  // const exported = {
+  //   data: filtered.reduce((a, c) => {
+  //     if (c.date !== toDateString(date)) {
+  //       return a;
+  //     }
+  //     return [
+  //       ...a,
+  //       {
+  //         "Govt/Pvt": GOVT_FACILITY_TYPES.includes(c.facilityType)
+  //           ? "Govt"
+  //           : "Pvt",
+  //         "Hops/CFLTC":
+  //           c.facilityType === "First Line Treatment Centre"
+  //             ? "CFLTC"
+  //             : "Hops" || null,
+  //         "Hospital/CFLTC Address": c.address || null,
+  //         "Hospital/CFLTC Name": c.name || null,
+  //         Mobile: c.phoneNumber || null,
+  //         ...AVAILABILITY_TYPES_ORDERED.reduce((t, x) => {
+  //           const y = { ...t };
+  //           y[`Current ${AVAILABILITY_TYPES[x]}`] =
+  //             c.capacity[x]?.current_capacity || 0;
+  //           y[`Total ${AVAILABILITY_TYPES[x]}`] =
+  //             c.capacity[x]?.total_capacity || 0;
+  //           return y;
+  //         }, {}),
+  //       },
+  //     ];
+  //   }, []),
+  //   filename: "capacity_export.csv",
+  // };
+
+  return {
+    props: {
+      data,
+      // exported,
+    },
+  };
+};
