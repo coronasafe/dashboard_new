032cfcc17c18a9269c06d9c23dd20c877f0d4ec2
developedBySJ <developedbysj@gmail.com>
2021-09-02 17:58:11 +0530

    Add server side rendering to test page

--- pages/district/[districtName]/tests.tsx
+++ pages/district/[districtName]/tests.tsx
@@ -1,5 +1,4 @@
-// @ts-nocheck
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ContentNav from "../../../components/ContentNav";
 import { InfoCard } from "../../../components/InfoCard";
 import { ValuePill } from "../../../components/Pill";
@@ -9,24 +8,30 @@ import { ACTIVATED_DISTRICTS, TESTS_TYPES } from "../../../lib/common";
 import { data } from "../../../utils/mock/GenericTableData";
 import { ColumnType, DefaultRecordType } from "rc-table/lib/interface";
 import { GetServerSideProps } from "next";
-import { parameterize, toDateString } from "../../../utils/parser";
+import {
+  getNDateAfter,
+  getNDateBefore,
+  parameterize,
+  toDateString,
+} from "../../../utils/parser";
 import { careSummary } from "../../../lib/types";
-import { processFacilityData } from "../../../lib/common/processor";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import relativeTime from "dayjs/plugin/relativeTime";
+import {
+  processFacilityDataUpdate,
+  TestFacilitiesTrivia,
+} from "../../../lib/common/processor";
+import {
+  getTestTableData,
+  getTestTableRow,
+  processTestFacilitiesTriviaData,
+  TestTableData,
+} from "../../../lib/common/processor/testsProcessor";
+import { Pagination } from "@windmill/react-ui";
 
 dayjs.extend(relativeTime);
 dayjs.extend(customParseFormat);
-
-const initialFacilitiesTrivia = {
-  count: 0,
-  result_awaited: 0,
-  test_discarded: 0,
-  total_patients: 0,
-  result_negative: 0,
-  result_positive: 0,
-};
 
 const columns: ColumnType<DefaultRecordType>[] = [
   {
@@ -80,38 +85,53 @@ const columns: ColumnType<DefaultRecordType>[] = [
   },
 ];
 
-const Tests = () => {
-  const TableRow = (name: string, type: string, phone: string) => {
-    return (
-      <div>
-        <a href="#">{name}</a>
-        <p className="text-gray-400 mb-2">{type}</p>
-        <p>{phone}</p>
-      </div>
+interface TestsProps {
+  facilityTrivia: TestFacilitiesTrivia;
+  tableData: TestTableData[];
+}
+
+const Tests = ({ facilityTrivia, tableData: initialTableData }: TestsProps) => {
+  const [tableData, setTableData] = useState<TestTableData[]>([]);
+
+  const tableRows = getTestTableRow(tableData);
+
+  const [page, setPage] = useState(0);
+  const resultsPerPage = 10;
+
+  useEffect(() => {
+    console.log(":UPATE");
+    setTableData(
+      initialTableData.slice(page * resultsPerPage, (page + 1) * resultsPerPage)
     );
-  };
+  }, [initialTableData, page]);
 
   return (
     <div className="container mx-auto px-4">
       <ContentNav />
 
       <div className="grid gap-1 grid-rows-none mb-8 sm:grid-flow-col-dense sm:grid-rows-1 sm:place-content-end">
-        <ValuePill title="Facility Count" value={1231} />
-        <ValuePill title="Patient Count" value={432} />
+        <ValuePill
+          title="Facility Count"
+          value={facilityTrivia.current.count}
+        />
+        <ValuePill
+          title="Patient Count"
+          value={facilityTrivia.current.total_patients}
+        />
       </div>
       <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-8 ">
-        {Object.entries(TESTS_TYPES).map(([key, value], i) => {
+        {Object.entries(TESTS_TYPES).map(([k, val], i) => {
+          const key = k as keyof typeof TESTS_TYPES;
+          const title = val;
+          const delta =
+            facilityTrivia.current[key] - facilityTrivia.previous[key];
           if (key !== "total_patients") {
             return (
               <InfoCard
                 key={i}
-                title={value}
-                //@ts-ignore
-                value={Math.random() * 500 + 200}
-                delta={
-                  //@ts-ignore
-                  (Math.random() * 100 + 50) * (Math.random() > 0.5 ? -1 : 1)
-                }
+                title={title}
+                value={facilityTrivia.current[key]}
+                delta={delta}
               />
             );
           }
@@ -124,7 +144,15 @@ const Tests = () => {
           setSearchValue={() => {}}
           className="mb-2"
         />
-        <GenericTable columns={columns} data={data} />
+        <GenericTable columns={columns} data={tableRows} />
+        <div className="mt-4">
+          <Pagination
+            resultsPerPage={10}
+            totalResults={initialTableData.length}
+            label=""
+            onChange={(page) => setPage(page)}
+          />
+        </div>
       </div>
     </div>
   );
@@ -142,40 +170,23 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     };
   }
 
-  const date = new Date();
-  const data = await careSummary("tests", district.id);
+  const today = new Date();
+  const start_date = toDateString(getNDateBefore(today, 1));
+  const end_date = toDateString(getNDateAfter(today, 1));
+  const limit = 2000;
 
-  console.log(data);
-
-  const filtered = processFacilityData(data?.results);
-  const facilityTrivia = filtered.reduce(
-    (a, c) => {
-      const key = c.date === toDateString(date) ? "current" : "previous";
-      a[key].count += 1;
-      Object.keys(TESTS_TYPES).forEach((k) => {
-        a[key][k] += c[k];
-        a[key][k] += c[k];
-      });
-      return a;
-    },
-    {
-      current: JSON.parse(JSON.stringify(initialFacilitiesTrivia)),
-      previous: JSON.parse(JSON.stringify(initialFacilitiesTrivia)),
-    }
+  const data = await careSummary(
+    "tests",
+    district.id,
+    limit,
+    start_date,
+    end_date
   );
-  const tableData = filtered.reduce((a, c) => {
-    if (c.date !== toDateString(date)) {
-      return a;
-    }
-    return [
-      ...a,
-      [
-        [c.name, c.facilityType, c.phoneNumber],
-        dayjs(c.modifiedDate, "DD-MM-YYYY HH:mm").fromNow(),
-        ...Object.keys(TESTS_TYPES).map((i) => c[i]),
-      ],
-    ];
-  }, []);
+
+  const filtered = processFacilityDataUpdate(data.results);
+
+  const facilityTrivia = processTestFacilitiesTriviaData(filtered);
+  const tableData = getTestTableData(filtered);
 
   return {
     props: {
