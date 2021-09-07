81bae1337b1054947e62ddb40c52be839437d958
developedBySJ <developedbysj@gmail.com>
2021-09-07 21:31:09 +0530

    Refactor

--- components/OxygenFacilityCard/OxygenFacilityCard.tsx
+++ components/OxygenFacilityCard/OxygenFacilityCard.tsx
@@ -1,28 +1,41 @@
 import React from "react";
-import { columns, data } from "../../utils/mock/OxygenTableData";
+import {
+  getOxygenTableRows,
+  OxygenCardData,
+} from "../../lib/common/processor/oxygenDataProcessor";
+import { columns } from "../../utils/mock/OxygenTableData";
 import { GenericTable } from "../Table";
 
 interface OxygenFacilityCardProps {
   className?: string;
+  data: OxygenCardData;
 }
 export const OxygenFacilityCard: React.FC<OxygenFacilityCardProps> = ({
   className,
+  data,
 }) => {
+  const { facility_name, facility_last_updated, facility_type, phone_number } =
+    data;
+  const tableData = getOxygenTableRows(data);
   return (
     <div className={className}>
       <div className="fle">
-        <h1 className="dark:text-white text-3xl">SANGEETH HOSPITAL</h1>
-        <h1 className="text-base text-gray-400">Updated An Hour Ago</h1>
+        <h1 className="dark:text-white text-3xl">{facility_name}</h1>
+        <h1 className="text-base text-gray-400">{facility_last_updated}</h1>
       </div>
       <div className="my-4">
         <span className="text-sm dark:bg-indigo-900 bg-indigo-200 rounded-full py-1 px-2 text-black dark:text-white">
-          Private Hospital
+          {facility_type}
         </span>
         <span className="text-sm text-black dark:text-white">
-          +91 90127897812
+          {phone_number}
         </span>
       </div>
-      <GenericTable columns={columns} data={data} scroll={{ x: "900px" }} />
+      <GenericTable
+        columns={columns}
+        data={tableData}
+        scroll={{ x: "900px" }}
+      />
     </div>
   );
 };
